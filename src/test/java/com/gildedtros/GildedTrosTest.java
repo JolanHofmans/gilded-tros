b520556ade9e2a4ce219bf19a8233a175042a97f
jolanhofmans <jolan.hofmans@qmino.com>
2025-02-28 17:55:00 +0100

    Refactor GildedTrosTest

--- src/test/java/com/gildedtros/GildedTrosTest.java
+++ src/test/java/com/gildedtros/GildedTrosTest.java
@@ -1,8 +1,10 @@
 package com.gildedtros;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -12,132 +14,84 @@ class GildedTrosTest {
     private static final String GOOD_WINE = "Good Wine";
     private static final String B_DAWG_KEYCHAIN = "B-DAWG Keychain";
 
-    @ParameterizedTest
-    @ValueSource(ints = {10, 1})
-    void regularItemShouldDecreaseInQualityByOneEachDay(int sellIn) {
-        Item[] items = new Item[]{new Item(NORMAL_ITEM, sellIn, 10)};
+    @Test
+    void regularItemShouldDecreaseInQualityByOneEachDay() {
+        Item[] items = new Item[]{new Item(NORMAL_ITEM, 1, 4)};
         GildedTros app = new GildedTros(items);
+
         app.updateQuality();
 
-        assertThat(app.items[0].quality).isEqualTo(9);
-        assertThat(app.items[0].sellIn).isEqualTo(sellIn - 1);
-    }
+        assertThat(app.items[0].quality).isEqualTo(3);
+        assertThat(app.items[0].sellIn).isZero();
 
-    @ParameterizedTest
-    @ValueSource(ints = {1, 0, -1})
-    void regularItemShouldNotDecreaseInQualityBelowZero(int sellIn) {
-        Item[] items = new Item[]{new Item(NORMAL_ITEM, sellIn, 0)};
-        GildedTros app = new GildedTros(items);
+        app.updateQuality();
+
+        assertThat(app.items[0].quality).isEqualTo(1);
+        assertThat(app.items[0].sellIn).isEqualTo(-1);
+
         app.updateQuality();
 
         assertThat(app.items[0].quality).isZero();
-        assertThat(app.items[0].sellIn).isEqualTo(sellIn - 1);
+        assertThat(app.items[0].sellIn).isEqualTo(-2);
     }
 
-    @ParameterizedTest
-    @ValueSource(ints = {0, -1})
-    void regularItemShouldDecreaseInQualityByTwoAfterHittingSellInDay(int sellIn) {
-        Item[] items = new Item[]{new Item(NORMAL_ITEM, sellIn, 10)};
+    @Test
+    void goodWineShouldIncreaseInQualityByOneEachDay() {
+        Item[] items = new Item[]{new Item(GOOD_WINE, 1, 47)};
         GildedTros app = new GildedTros(items);
+
         app.updateQuality();
 
-        assertThat(app.items[0].quality).isEqualTo(8);
-        assertThat(app.items[0].sellIn).isEqualTo(sellIn - 1);
-    }
+        assertThat(app.items[0].quality).isEqualTo(48);
+        assertThat(app.items[0].sellIn).isZero();
 
-    @ParameterizedTest
-    @ValueSource(ints = {10, 1})
-    void goodWineShouldIncreaseInQualityByOneEachDay(int sellIn) {
-        Item[] items = new Item[]{new Item(GOOD_WINE, sellIn, 10)};
-        GildedTros app = new GildedTros(items);
         app.updateQuality();
 
-        assertThat(app.items[0].quality).isEqualTo(11);
-        assertThat(app.items[0].sellIn).isEqualTo(sellIn - 1);
-    }
+        assertThat(app.items[0].quality).isEqualTo(50);
+        assertThat(app.items[0].sellIn).isEqualTo(-1);
 
-    @ParameterizedTest
-    @ValueSource(ints = {0, -10})
-    void goodWineShouldIncreaseInQualityByTwoAfterSellIn(int sellIn) {
-        Item[] items = new Item[]{new Item(GOOD_WINE, sellIn, 10)};
-        GildedTros app = new GildedTros(items);
         app.updateQuality();
 
-        assertThat(app.items[0].quality).isEqualTo(12);
-        assertThat(app.items[0].sellIn).isEqualTo(sellIn - 1);
+        assertThat(app.items[0].quality).isEqualTo(50);
+        assertThat(app.items[0].sellIn).isEqualTo(-2);
     }
 
-    @ParameterizedTest
-    @ValueSource(ints = {10, 0, -10})
-    void bDawgShouldStayConstant(int sellIn) {
-        Item[] items = new Item[]{new Item(B_DAWG_KEYCHAIN, sellIn, 10)};
+    @Test
+    void bDawgShouldStayConstant() {
+        Item[] items = new Item[]{new Item(B_DAWG_KEYCHAIN, 1, 10)};
         GildedTros app = new GildedTros(items);
         app.updateQuality();
 
         assertThat(app.items[0].quality).isEqualTo(10);
-        assertThat(app.items[0].sellIn).isEqualTo(sellIn);
+        assertThat(app.items[0].sellIn).isEqualTo(1);
     }
 
     @ParameterizedTest
-    @CsvSource({
-            "Backstage passes for Re:Factor, 11",
-            "Backstage passes for HAXX, 11",
+    @ValueSource(strings = {
+            "Backstage passes for Re:Factor",
+            "Backstage passes for HAXX",
     })
-    void backStagePassesShouldDecreaseByOneWhenWellInLongerThanTenDays(String backstagePass, int sellIn) {
-        Item[] items = new Item[]{new Item(backstagePass, sellIn, 10)};
+    void backStagePassesShouldDecreaseByOneWhenWellInLongerThanTenDays(String backstagePass) {
+        Item[] items = new Item[]{new Item(backstagePass, 15, 2)};
         GildedTros app = new GildedTros(items);
+
+        IntStream.range(0, 5).forEach(i -> app.updateQuality());
+        assertThat(app.items[0].quality).isEqualTo(7);
+        assertThat(app.items[0].sellIn).isEqualTo(10);
+
+        IntStream.range(0, 5).forEach(i -> app.updateQuality());
+        assertThat(app.items[0].quality).isEqualTo(17);
+        assertThat(app.items[0].sellIn).isEqualTo(5);
+
+        IntStream.range(0, 5).forEach(i -> app.updateQuality());
+        assertThat(app.items[0].quality).isEqualTo(32);
+        assertThat(app.items[0].sellIn).isZero();
+
         app.updateQuality();
-
-        assertThat(app.items[0].quality).isEqualTo(11);
-        assertThat(app.items[0].sellIn).isEqualTo(sellIn - 1);
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-            "Backstage passes for Re:Factor, 10",
-            "Backstage passes for Re:Factor, 6",
-            "Backstage passes for HAXX, 10",
-            "Backstage passes for HAXX, 6",
-    })
-    void backstagePassesForInterestingConfsShouldIncreaseByTwoWhenWellInLongerThanTenDays(String backstagePass, int sellIn) {
-        Item[] items = new Item[]{new Item(backstagePass, sellIn, 10)};
-        GildedTros app = new GildedTros(items);
-        app.updateQuality();
-
-        assertThat(app.items[0].quality).isEqualTo(12);
-        assertThat(app.items[0].sellIn).isEqualTo(sellIn - 1);
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-            "Backstage passes for Re:Factor, 5",
-            "Backstage passes for Re:Factor, 1",
-            "Backstage passes for HAXX, 5",
-            "Backstage passes for HAXX, 1",
-    })
-    void backstagePassesForInterestingConfsShouldIncreaseByThreeWhenWellInLongerThanTenDays(String backstagePass, int sellIn) {
-        Item[] items = new Item[]{new Item(backstagePass, sellIn, 10)};
-        GildedTros app = new GildedTros(items);
-        app.updateQuality();
-
-        assertThat(app.items[0].quality).isEqualTo(13);
-        assertThat(app.items[0].sellIn).isEqualTo(sellIn - 1);
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-            "Backstage passes for Re:Factor, 0",
-            "Backstage passes for Re:Factor, -1",
-            "Backstage passes for HAXX, 0",
-            "Backstage passes for HAXX, -1",
-    })
-    void backstagePassesForInterestingConfsShouldBeZeroQualityWhenHittingSellIn(String backstagePass, int sellIn) {
-        Item[] items = new Item[]{new Item(backstagePass, sellIn, 10)};
-        GildedTros app = new GildedTros(items);
-        app.updateQuality();
-
         assertThat(app.items[0].quality).isZero();
-        assertThat(app.items[0].sellIn).isEqualTo(sellIn - 1);
+        assertThat(app.items[0].sellIn).isEqualTo(-1);
+
+
     }
 
     @ParameterizedTest
@@ -165,26 +119,6 @@ class GildedTrosTest {
 
         assertThat(app.items[0].quality).isZero();
         assertThat(app.items[0].sellIn).isEqualTo(-3);
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-            "Backstage passes for Re:Factor, 5, 48",
-            "Backstage passes for Re:Factor, 10, 49",
-            "Backstage passes for Re:Factor, 15, 50",
-            "Backstage passes for HAXX, 5, 48",
-            "Backstage passes for HAXX, 10, 49",
-            "Backstage passes for HAXX, 15, 50",
-            "Good Wine, 10, 49",
-            "Good Wine, -10, 49",
-    })
-    void itemShouldNotIncreaseInQualityOverFifty(String itemName, int sellIn, int quality) {
-        Item[] items = new Item[]{new Item(itemName, sellIn, quality)};
-        GildedTros app = new GildedTros(items);
-        app.updateQuality();
-
-        assertThat(app.items[0].quality).isEqualTo(50);
-        assertThat(app.items[0].sellIn).isEqualTo(sellIn - 1);
     }
 
 }
