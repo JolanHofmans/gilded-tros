f5fe99f14e8d524ea2a0022cc4cc8657feb9bbcc
jolanhofmans <jolan.hofmans@qmino.com>
2025-03-01 10:25:44 +0100

    Create map to link item names to product types

--- src/main/java/com/gildedtros/products/ProductFactory.java
+++ src/main/java/com/gildedtros/products/ProductFactory.java
@@ -3,6 +3,9 @@ package com.gildedtros.products;
 import com.gildedtros.Item;
 import com.gildedtros.products.strategies.*;
 
+import java.util.List;
+import java.util.Map;
+
 public class ProductFactory {
 
     private static final RegularDevaluationQualityUpdatingStrategy REGULAR_DEVALUATION = new RegularDevaluationQualityUpdatingStrategy();
@@ -11,16 +14,35 @@ public class ProductFactory {
     private static final EventPeakQualityUpdatingStrategy EVENT_PEAK = new EventPeakQualityUpdatingStrategy();
     private static final DoubleRateDevaluationQualityUpdatingStrategy DOUBLE_RATE_DEVALUATION = new DoubleRateDevaluationQualityUpdatingStrategy();
 
+    private static final Map<ProductType, List<String>> productTypeToNames = Map.of(
+            ProductType.GOOD_WINE, List.of("Good Wine"),
+            ProductType.RARE, List.of("B-DAWG Keychain"),
+            ProductType.BACKSTAGE_PASSES, List.of("Backstage passes for Re:Factor", "Backstage passes for HAXX"),
+            ProductType.SMELLY, List.of("Duplicate Code", "Long Methods", "Ugly Variable Names")
+    );
+
     private ProductFactory() {
     }
 
     public static Product createProduct(Item item) {
-        return switch (item.name) {
-            case "Good Wine" -> Product.createProduct(item.name, item.sellIn, item.quality, REGULAR_APPRECIATION);
-            case "B-DAWG Keychain" -> Product.createProduct(item.name, item.sellIn, CONSTANT_QUALITY);
-            case "Backstage passes for Re:Factor", "Backstage passes for HAXX" -> Product.createProduct(item.name, item.sellIn, item.quality, EVENT_PEAK);
-            case "Duplicate Code", "Long Methods", "Ugly Variable Names" -> Product.createProduct(item.name, item.sellIn, item.quality, DOUBLE_RATE_DEVALUATION);
-            default -> Product.createProduct(item.name, item.sellIn, item.quality, REGULAR_DEVALUATION);
+        return switch (getProductType(item.name)) {
+            case GOOD_WINE -> Product.createProduct(item.name, item.sellIn, item.quality, REGULAR_APPRECIATION);
+            case RARE -> Product.createProduct(item.name, item.sellIn, CONSTANT_QUALITY);
+            case BACKSTAGE_PASSES -> Product.createProduct(item.name, item.sellIn, item.quality, EVENT_PEAK);
+            case SMELLY -> Product.createProduct(item.name, item.sellIn, item.quality, DOUBLE_RATE_DEVALUATION);
+            case REGULAR -> Product.createProduct(item.name, item.sellIn, item.quality, REGULAR_DEVALUATION);
         };
+    }
+
+    private static ProductType getProductType(String name) {
+        return productTypeToNames.entrySet().stream()
+                .filter(entry -> entry.getValue().contains(name))
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(ProductType.REGULAR);
+    }
+
+    enum ProductType {
+        REGULAR, GOOD_WINE, RARE, BACKSTAGE_PASSES, SMELLY
     }
 }
